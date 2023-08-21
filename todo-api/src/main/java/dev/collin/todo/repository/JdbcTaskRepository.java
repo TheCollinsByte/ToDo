f7587c86cd7323833105a08e8e2a3fcff129993c
colspan <collin@collindigm.com>
2023-08-21 10:35:50 +0300

    Task: GetAllTask Method Implemented

--- todo-api/src/main/java/dev/collin/todo/repository/JdbcTaskRepository.java
+++ todo-api/src/main/java/dev/collin/todo/repository/JdbcTaskRepository.java
@@ -1,36 +1,50 @@
 package dev.collin.todo.repository;
 
 import com.google.common.base.Preconditions;
-import dev.collin.todo.config.SqliteConnection;
+import dev.collin.todo.config.DatabaseConnection;
 import dev.collin.todo.model.Task;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class JdbcTaskRepository implements ITaskRepository {
     private static final Logger LOG = LoggerFactory.getLogger(JdbcTaskRepository.class);
 
-    private final SqliteConnection sqliteConnection;
-
-    @Autowired
-    public JdbcTaskRepository(SqliteConnection sqliteConnection) {
-        this.sqliteConnection = Preconditions.checkNotNull(sqliteConnection);
-    }
-
     @Override
     public Task createTask(Task task) throws SQLException {
-        sqliteConnection.execute("INSERT INTO tasks(title) VALUES(?)");
         return task;
     }
 
     @Override
     public List<Task> getAllTask() throws SQLException {
-        return null;
+        List<Task> tasks = new ArrayList<>();
+        String query = "SELECT * FROM task";
+
+        try {
+            Connection connection = DatabaseConnection.getConnection();
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                Task task = new Task();
+                task.setId(resultSet.getLong("id"));
+                task.setTitle(resultSet.getString("title"));
+                task.setDescription(resultSet.getString("description"));
+                tasks.add(task);
+            }
+        } catch (SQLException e) {
+            LOG.error("SQL Exception:", e);
+        }
+
+        return tasks;
     }
 
     @Override
