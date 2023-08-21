f6d900e17f418a219b8488afe0b64eed6f442ba5
colspan <collin@collindigm.com>
2023-08-21 09:08:12 +0300

    Interface method task update

--- todo-api/src/main/java/dev/collin/todo/repository/ITaskRepository.java
+++ todo-api/src/main/java/dev/collin/todo/repository/ITaskRepository.java
@@ -1,11 +1,14 @@
 package dev.collin.todo.repository;
 
+import dev.collin.todo.model.Task;
+
 import java.sql.SQLException;
+import java.util.List;
 
 public interface ITaskRepository {
-    public String createTask() throws SQLException;
-    public String getTask() throws SQLException;
-    public String getTasks() throws SQLException;
-    public String updateTask() throws SQLException;
-    public String deleteTask() throws SQLException;
+    Task createTask(Task task) throws SQLException;
+    List<Task> getAllTask() throws SQLException;
+    Task getTaskById(Long id) throws SQLException;
+    Task updateTask(Long id, Task task) throws SQLException;
+    void deleteTask(Long id) throws SQLException;
 }
