fc628f3968aa475dc38975a7f2e499ce56a52e87
colspan <collin@collindigm.com>
2023-08-21 10:35:01 +0300

    DB Connection: To Get connection to the Database

--- todo-api/src/main/java/dev/collin/todo/config/DatabaseConnection.java
+++ todo-api/src/main/java/dev/collin/todo/config/DatabaseConnection.java
@@ -3,10 +3,28 @@ package dev.collin.todo.config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.sqlite.SQLiteConfig;
 
-@Component
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
 
     private static final Logger LOG = LoggerFactory.getLogger(DatabaseConnection.class);
 
+    private static final String URL = "jdbc:sqlite:src/main/resources/todo.sqlite";
+    private static Connection connection;
+
+    public static Connection getConnection() {
+        if (connection == null) {
+            try {
+                Class.forName("org.sqlite.JDBC");
+                connection = DriverManager.getConnection(URL);
+            } catch (ClassNotFoundException | SQLException e) {
+                LOG.error("Connection Not Established", e);
+            }
+        }
+        return connection;
+    }
 }
