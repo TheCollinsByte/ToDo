654300fff59f39e43740e8244b1fa030a048ed50
colspan <collin@collindigm.com>
2023-08-21 12:10:42 +0300

    Permitting all permitting cross-origin requests on Task Endpoints

--- todo-api/src/main/java/dev/collin/todo/controller/TaskController.java
+++ todo-api/src/main/java/dev/collin/todo/controller/TaskController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/v1/todo")
 public class TaskController {
 
