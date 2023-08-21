2ed49074c5da7d96fa0079fb16b587de25165feb
colspan <collin@collindigm.com>
2023-08-21 19:06:07 +0300

    Task List Component Get from the Server

---
+++ todo-webapp/src/components/TaskList.js
@@ -0,0 +1,25 @@
+import React, {useState} from "react";
+export const TaskList = () => {
+
+    const [tasks, setTasks] = useState([]);
+
+    export const fetchTasks = async () => {
+        try {
+            const response = await fetch('http://localhost:8080/v1/todo/tasks');
+            const data = await response.json();
+            setTasks(data);
+        } catch (error) {
+            console.error('Error fetching tasks:', error);
+        }
+    };
+
+
+    return (
+        <div>
+            <h1>Tasks</h1>
+            {tasks.map((e) => {
+                console.log(e)
+            })}
+        </div>
+    )
+}
