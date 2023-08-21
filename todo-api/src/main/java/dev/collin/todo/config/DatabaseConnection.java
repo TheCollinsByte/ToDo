a2cb0f86926d26754ed0de72d23aca602c6bc9a6
colspan <collin@collindigm.com>
2023-08-21 09:47:23 +0300

    TODO: DB Connection

---
+++ todo-api/src/main/java/dev/collin/todo/config/DatabaseConnection.java
@@ -0,0 +1,12 @@
+package dev.collin.todo.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseConnection {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DatabaseConnection.class);
+
+}
