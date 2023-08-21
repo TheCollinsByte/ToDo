70b2ee80e1bee329967040b10e670ef20dc5d796
colspan <collin@collindigm.com>
2023-08-21 09:58:32 +0300

    Task Class Clean-up

--- todo-api/src/main/java/dev/collin/todo/model/Task.java
+++ todo-api/src/main/java/dev/collin/todo/model/Task.java
@@ -38,20 +38,6 @@ public class Task {
 
     private String description;
 
-    private Time dueDate;
-
-    private String priority;
-
-    private String status;
-
-    private String category;
-
-    private String tags;
-
-    private Instant creationDate;
-
-    private Time completionDate;
-
     public Long getId() {
         return id;
     }
@@ -76,75 +62,12 @@ public class Task {
         this.description = description;
     }
 
-    public Time getDueDate() {
-        return dueDate;
-    }
-
-    public void setDueDate(Time dueDate) {
-        this.dueDate = dueDate;
-    }
-
-    public String getPriority() {
-        return priority;
-    }
-
-    public void setPriority(String priority) {
-        this.priority = priority;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public String getTags() {
-        return tags;
-    }
-
-    public void setTags(String tags) {
-        this.tags = tags;
-    }
-
-    public Instant getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(Instant creationDate) {
-        this.creationDate = creationDate;
-    }
-
-    public Time getCompletionDate() {
-        return completionDate;
-    }
-
-    public void setCompletionDate(Time completionDate) {
-        this.completionDate = completionDate;
-    }
-
     @Override
     public String toString() {
         return "Task{" +
                 "id=" + id +
                 ", title='" + title + '\'' +
                 ", description='" + description + '\'' +
-                ", dueDate=" + dueDate +
-                ", priority='" + priority + '\'' +
-                ", status='" + status + '\'' +
-                ", category='" + category + '\'' +
-                ", tags='" + tags + '\'' +
-                ", creationDate=" + creationDate +
-                ", completionDate=" + completionDate +
                 '}';
     }
 }
