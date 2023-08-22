eb2665e61b288fef0331ecc571ae64bf20215326
colspan <collin@collindigm.com>
2023-08-22 08:50:57 +0300

    App Component Clean-up

--- todo-webapp/src/components/App.js
+++ todo-webapp/src/components/App.js
@@ -1,12 +1,10 @@
-import {AddTask} from "./AddTask";
-import {TaskList} from "./TaskList";
+import {TodoList} from "./task/TodoList";
 
 function App () {
     return (
        <div>
            <h1 style={{textAlign: "center", color: "blue"}}>TO-Do Web App</h1>
-           <AddTask />
-           <TaskList />
+           <TodoList />
        </div>
     )
 }
