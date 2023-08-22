99d91ee4d2579de0f8f6c4135e8b72b34fcc6c8e
colspan <collin@collindigm.com>
2023-08-22 19:27:10 +0300

    component Clean up

--- todo-webapp/src/components/task/TaskList.js
+++ todo-webapp/src/components/task/TaskList.js
@@ -1,33 +1,19 @@
-import React, {useState} from "react";
-export const TaskList = () => {
-
-    const [tasks, setTasks] = useState([]);
-
-    const fetchTasks = async () => {
-        try {
-            const response = await fetch('http://localhost:8080/v1/todo/tasks');
-            const data = await response.json();
-            setTasks(data);
-        } catch (error) {
-            console.error('Error fetching tasks:', error);
-        }
-    };
-
+export const TaskList = ({ tasks, toggleTask, deleteTask }) => {
 
     return (
         <div>
             <ul>
                 {tasks.map((task) => (
-                    <li key={task.id}>
+                    <li key={task.title}>
                         <input
                             type="checkbox"
                             checked={false}
-                            onChange={() => console.log("Input Checkbox")}
+                            onChange={() => toggleTask}
                         />
                         <span>
                           {task.title} - {task.description}
                         </span>
-                        <button onClick={() => console.log("Delete Button")}>Delete</button>
+                        <button onClick={() => deleteTask}>Delete</button>
                     </li>
                 ))}
             </ul>
